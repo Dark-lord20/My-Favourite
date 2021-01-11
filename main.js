b88ff20dfb35b45ecb6dc5f8f3fa9203773b28bd
Dark-lord20 <73019620+Dark-lord20@users.noreply.github.com>
2021-01-11 17:17:45 +0530

    f

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function collaps(){
-    if (document.getElementById("col").style.opacity==="0"){
-      document.getElementById("col").style.opacity="1"
+    if (document.getElementById("col").style.opacity=="0"){
+      console.log("hello world");
+      document.getElementById("col").style.opacity="1";
     }
     else {
-      document.getElementById("col").style.opacity="0"
+      document.getElementById("col").style.opacity="0";
     }
   }
